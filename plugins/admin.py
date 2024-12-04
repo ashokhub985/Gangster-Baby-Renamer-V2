c5888300e4601db47281127f9c07660e9303769a
ashokhub985 <ar5190669@gmail.com>
2024-12-04 22:47:38 +0530

    Update admin.py

--- plugins/admin.py
+++ plugins/admin.py
@@ -4,7 +4,7 @@ from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, ForceRepl
 from helper.database import uploadlimit, usertype, addpre, find_one, botdata
 from helper.progress import humanbytes
 
-ADMIN = int(os.environ.get("ADMIN", ))
+ADMIN = int(os.environ.get("ADMIN", 862729509))
 LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002247619392"))
 
 @Client.on_message(filters.private & filters.user(ADMIN) & filters.command(["warn"]))
